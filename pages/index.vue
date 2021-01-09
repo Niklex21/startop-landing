4004a4c4fb405343447ee16bfafa08fc8e9bc1a6
Alex Nikanov <alekseynikanov.21@gmail.com>
2021-01-09 03:24:11 +0200

    bug: Added forgotten name in input in form

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
       <div class="w-full">
         <h3 class="text-red-500 mb-2 text-center font-mono text-lg md:text-2xl">Subscribe to our <a class="border-b-2 border-current font-bold" href="https://nuoverload.substack.com/">newsletter</a>:</h3>
         <form method="POST" action="/?success=true" name="subscription-form" class="flex w-full border-red-500 border-2 border-solid rounded-sm" data-netlify="true" data-netlify-honeypot="bot-field">
-            <input class="focus:outline-none text-red-500 text-center align-middle w-full px-4 h-12 md:h-20 text-lg md:text-3xl placeholder-red-500 placeholder-opacity-40 bg-transparent" placeholder="your@mail.com" type="email" />
+            <input class="focus:outline-none text-red-500 text-center align-middle w-full px-4 h-12 md:h-20 text-lg md:text-3xl placeholder-red-500 placeholder-opacity-40 bg-transparent" placeholder="your@mail.com" name="email" type="email" />
             <button class="bg-red-500 text-center text-red-900 font-bold align-middle uppercase w-auto min-w-min px-4 md:px-12 h-12 md:h-20 text-xl md:text-2xl" type="submit">
               <svg class="h-7 block md:h-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
