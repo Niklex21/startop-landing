0399397b7288344b88e35bfc19f24f999017d878
Alex Nikanov <alekseynikanov.21@gmail.com>
2021-01-21 00:51:39 +0200

    Fixed background issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>  
-  <div>
-    <div id="background" class="absolute top-0 w-screen overflow-hidden bg-no-repeat bg-center bg-fixed h-screen" style="background-image: url('/index_bg_1.jpg')">
+  <div class=" text-red-main">
+    <div id="background" class="fixed top-0 w-screen overflow-hidden bg-no-repeat bg-center bg-fixed h-screen" style="background-image: url('/index_bg_1.jpg')">
         <div class="absolute top-0 h-screen bg-fixed w-screen bg-black bg-opacity-70"></div>
     </div>
     <div class="absolute top-0 w-full font-futura">
@@ -9,19 +9,28 @@
           <img class="block h-28 md:h-40" src="/logo.png" alt="logo" />
         </div>
         <div id="text" class="w-full">
-          <h1 class="w-full text-red-main font-bold text-6xl md:text-8xl text-center">The Boston College Startup Newsletter</h1>
+          <h1 class="w-full font-bold text-6xl md:text-8xl text-center">The Boston College Startup Newsletter</h1>
         </div>
         <div class="w-full h-28 md:h-40 flex flex-wrap content-end">
-          <a class="w-full" href="https://startop.substack.com/subscribe"><h3 class="w-full border-2 border-solid p-4 text-center text-red-main border-red-main uppercase text-2xl md:text-4xl">Subscribe to our <br class="block md:hidden"> newsletter here</h3></a>
+          <a class="w-full" href="https://startop.substack.com/subscribe"><h3 class="w-full border-2 border-solid p-4 text-center border-red-main uppercase text-2xl md:text-4xl">Subscribe to our <br class="block md:hidden"> newsletter here</h3></a>
         </div>
       </div>
       <div id="screen-2" class="content-between relative flex flex-wrap p-8 md:p-12 justify-around align-middle items-center h-screen w-full">
-        <h2 class="w-full text-red-main font-bold text-5xl text-center">Why subscribe?</h2>
-        <ul>
-          <li>Stay up to date with the trends</li>
-          <li>Work opportunities</li>
-          <li>Get inspired!</li>
-        </ul>
+        <h2 class="w-full font-bold text-5xl text-center">Why subscribe?</h2>
+        <div class="grid grid-cols-3 gap-4 text-3xl align-middle items-center justify-items-left">
+          <svg class="list-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+          </svg>
+          <span class="col-span-2">Stay up to date with the trends</span>
+          <svg class="list-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+          </svg>
+          <span class="col-span-2">Work opportunities</span>
+          <svg class="list-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+          </svg>
+          <span class="col-span-2">Get inspired!</span>
+        </div>
       </div>
     </div>
   </div>
@@ -32,3 +41,4 @@
 
   export default Vue.extend({}) 
 </script>
+
