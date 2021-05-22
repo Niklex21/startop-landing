3b7842326974adeee280238680da9f9067392478
Alex Nikanov <alekseynikanov.21@gmail.com>
2021-05-22 21:40:07 +0300

    feat: :sparkles: Added an admin page redirect

---
+++ pages/admin.vue
@@ -0,0 +1,10 @@
+<template>
+</template>
+
+<script>
+  export default {
+    middleware({ store, redirect }) {
+      return redirect('https://startop-cms.herokuapp.com/admin');
+    }
+  }
+</script>
