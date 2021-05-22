2e450f247f8a5a9ef6398caea90be8c466a341a3
Alex Nikanov <alekseynikanov.21@gmail.com>
2021-05-22 21:39:42 +0300

    refactor: :recycle: Changed the <a> local link tags to NuxtLink in all places

--- components/MainNavBar.vue
+++ components/MainNavBar.vue
@@ -1,15 +1,15 @@
 <template>
     <div id="header" class="flex content-center justify-center items-center md:content-start md:justify-items-start md:justify-between md:items-start w-full h-auto">
-        <a href="/">
+        <NuxtLink to="/">
             <img class="block mx-auto lg:hidden h-28 md:h-40" src="/logo.png" alt="logo" />
             <img class="hidden lg:block h-20" src="/banner.png" alt="banner" />
-        </a>
+        </NuxtLink>
         <nav class="hidden uppercase font-medium h-20 lg:flex text-right items-center text-3xl">
             <div class="h-auto w-auto">
                 <a class="m-2 font-bold hover-border-b-2" href="https://startop.substack.com">newsletter</a>
-                <a class="m-2 hover-border-b-2" href="/">home</a>
-                <a class="m-2 hover-border-b-2" href="/blog">blog</a>
-                <a class="m-2 hover-border-b-2" href="#screen-footer">contact</a>
+                <NuxtLink class="m-2 hover-border-b-2" to="/">home</NuxtLink>
+                <NuxtLink class="m-2 hover-border-b-2" to="/blog">blog</NuxtLink>
+                <NuxtLink class="m-2 hover-border-b-2" to="/#screen-footer">contact</NuxtLink>
             </div>
         </nav>
     </div>
